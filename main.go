3d750140a8c53bcc13bbff958af262fb9c3b0111
Julian Pearce <julian@aimusic.co.uk>
2020-11-14 12:16:29 +0000

    fix(concurrency): now works concurrently

--- main.go
+++ main.go
@@ -1,17 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 func main() {
-	services := []string{"https://google.com", "https://amazon.com", "https://netflix.com"}
-	for _, service := range services {
-		c := make(chan map[string]interface{})
-		go checkServiceStatus(service, c)
-		fmt.Println(<-c)
+	wg := sync.WaitGroup{}
+	services := []string{"https://amazon.com", "https://google.com", "https://netflix.com"}
+	for i := range services {
+		wg.Add(1)
+		go func(service string) {
+			defer wg.Done()
+			checkServiceStatus(service)
+		}(services[i])
 	}
+	wg.Wait()
 }
 
-func checkServiceStatus(service string, c chan map[string]interface{}) {
+func checkServiceStatus(service string) {
 	statusCode := getHTTPStatusCode(service)
-	c <- logStatus(service, statusCode)
+	fmt.Println(logStatus(service, statusCode))
 }
